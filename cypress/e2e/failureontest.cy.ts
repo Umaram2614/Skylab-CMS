f8b8297e11995e4a0a4aa1e53428d917376137ce
Uma <umaram2694@gmail.com>
2023-04-25 12:48:49 +0530

    REmoving space

--- cypress/e2e/failureontest.cy.ts
+++ cypress/e2e/failureontest.cy.ts
@@ -1,13 +1,10 @@
 describe('Skipping Cypress tests on first failure and save resources', () => {
-
 it("With Error", () => {
 cy.visit("https://demoqa.com/profile").screenshot();
 Cypress.on("uncaught:exception", (err, runnable) => {
     console.log("Exception Handled")
+    console.log("hello")
     return false;
     });
-
-
-
 });
 })
